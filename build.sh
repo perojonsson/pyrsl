b0b9a810bcc7327e546bb8457f604c284bc0ca6d
John Törnblom <john.tornblom@gmail.com>
2015-06-22 22:38:52 +0200

    initial commit of executable and packaged python version of gen_erate.exe.
    rely on the shebang /#!usr/bin/env python

---
+++ build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+CURR_DIR=$(pwd)
+TEMP_DIR=$(mktemp -d)
+function cleanup {
+    rm -rf $TEMP_DIR
+}
+trap cleanup EXIT
+
+function install_ply {
+    cd $TEMP_DIR
+    wget -qO - https://github.com/dabeaz/ply/archive/master.tar.gz | tar xzf -
+
+    cd ply-master
+    cp -r ply $1
+}
+
+function install_pyxtuml {
+    cd $TEMP_DIR
+    wget -qO - https://github.com/john-tornblom/pyxtuml/archive/master.tar.gz | tar xzf -
+
+    cd pyxtuml-master
+    python setup.py prepare
+    cp -r xtuml $1
+}
+
+function install_pyrsl {
+    cd $TEMP_DIR
+    wget -qO - https://github.com/john-tornblom/pyrsl/archive/master.tar.gz | tar xzf -
+
+    cd pyrsl-master
+    python setup.py prepare
+    cp -r rsl $1
+}
+
+function install_main {
+    echo "from rsl import legacy_cli" >  $1/__main__.py
+    echo "legacy_cli.main()"          >> $1/__main__.py
+}
+
+
+function make_executable {
+    python -m zipfile -c $TEMP_DIR/pkg.zip $1/*
+    echo "#!/usr/bin/env python" > $2
+    cat $TEMP_DIR/pkg.zip >> $2
+    chmod +x $2
+}
+
+
+mkdir $TEMP_DIR/inst || exit 1
+
+echo ""
+echo "Fetching from github and packaging, please wait..."
+echo ""
+
+install_main    $TEMP_DIR/inst || exit 1
+install_ply     $TEMP_DIR/inst || exit 1
+install_pyxtuml $TEMP_DIR/inst || exit 1
+install_pyrsl   $TEMP_DIR/inst || exit 1
+
+make_executable $TEMP_DIR/inst $CURR_DIR/gen_erate || exit 1
+
+echo ""
+echo "Executable successfully created"
+echo ""
+echo "    $CURR_DIR/gen_erate"
